18996cd727eb12c8d4890a94adf4b3cb4543067f
Lawpreet2689 <singhlawpreet2689@gmail.com>
2026-05-15 16:43:17 +0530

    Restore hotel menu and order processing
    Reintroduce menu and order logic for hotel items.

--- hotel.py
+++ hotel.py
@@ -13,7 +13,7 @@ order_total = 0
 item_1 = input("Enter the 1st Item:")
 if item_1 in menu:
     order_total += menu[item_1]
-    print("Your item {item_1} has been added")
+    print(f"Your item {item_1} has been added")
 else:
     print(f"Ordered item {item_1} is not available")
 another_order = input("Do you want something else? (Yes/No)")
@@ -21,7 +21,7 @@ if another_order == "Yes":
     item_2 = input("Enter the 2nd Item:")
     if item_2 in menu:
         order_total += menu[item_2]
-        print("Your item {item_2} has been added")
+        print(f"Your item {item_2} has been added")
     else:
         print(f"Ordered item {item_2} is not available")
 
